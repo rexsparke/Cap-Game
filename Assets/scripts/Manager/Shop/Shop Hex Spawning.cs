3af1c2b603e18699337c2f14afdf6ad1d9a3143e
Lord-Cat-II <115671773+Lord-Cat-II@users.noreply.github.com>
2025-10-02 11:49:11 -0400

    Shop Setup Done, still needs script

---
+++ Assets/scripts/Manager/Shop/Shop Hex Spawning.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopHexSpawning : MonoBehaviour
+{
+    void Start()
+    {
+        InitialStock();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void InitialStock() //Restocks the shop with 3 new tiles, happens at the start of every build phase
+    {
+
+    }
+}
