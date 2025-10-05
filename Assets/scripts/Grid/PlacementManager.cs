e9312f989d48af2d70aaa391b902c8574b116e86
rexsparke <kirito5115101@gmail.com>
2025-10-05 10:40:50 -0400

    hex_debug

--- Assets/scripts/Grid/PlacementManager.cs
+++ Assets/scripts/Grid/PlacementManager.cs
@@ -9,11 +9,21 @@ public class PlacementManager : MonoBehaviour
     //private Grid m_hexGrid;
     //[SerializeField]
     //private GameObject m_hexLandPreFab;
-    //[SerializeField]
-    //private LayerMask m_groundMask;
     public Tilemap hexTileMap;
     public Tile selectedTile;
 
+    void Start()
+    {
+        if (hexTileMap != null && selectedTile != null)
+        {
+            Debug.Log("Placing test tile at (0,0)");
+            hexTileMap.SetTile(Vector3Int.zero, selectedTile);
+        }
+        else
+        {
+            Debug.LogError("Tilemap or Tile not assigned!");
+        }
+    }
     void Update()
     {
         //raycast from from camera
@@ -25,21 +35,46 @@ public class PlacementManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            Debug.Log("Mouse clicked!");
+                Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(
+                    new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0f));
 
-            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
-            {
-                Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                //Vector2 mouseWorldPos =;
-                Vector3Int cellPos = hexTileMap.WorldToCell(mouseWorldPos);
+            Vector3Int cellPos = hexTileMap.WorldToCell(mouseWorldPos);
+            Vector3 cellWorldPos = hexTileMap.CellToWorld(cellPos);
 
-                if (hexTileMap.HasTile(cellPos))
-                {
-                    hexTileMap.SetTile(cellPos, selectedTile);
-                }
-            }
-            
+            Debug.Log("Mouse world pos: " + mouseWorldPos + " | Cell pos: " + cellPos);
+
+            // Place Tile
+            hexTileMap.SetTile(cellPos, selectedTile);
+
+            //Draw debug square
+            DrawCellOutline(cellWorldPos);
+            //if(hexTileMap == null)
+            //{
+            //    Debug.LogError("HexTileMap is not assigned!");
+            //}
+            //if(selectedTile == null)
+            //{
+            //    Debug.LogError("SelectedTile is not assigned!");
+            //}
         }
+
+    }
+    void DrawCellOutline(Vector3 worldPos)
+    {
+        // Get cell size (usually 1x1 unless you changed Tilemap cell size)
+        Vector3 cellSize = hexTileMap.cellSize;
+
+        // Four corners of the cell
+        Vector3 bottomLeft = worldPos;
+        Vector3 bottomRight = worldPos + new Vector3(cellSize.x, 0, 0);
+        Vector3 topLeft = worldPos + new Vector3(0, cellSize.y, 0);
+        Vector3 topRight = worldPos + new Vector3(cellSize.x, cellSize.y, 0);
+
+        // Draw lines in the Scene view (visible in Play Mode)
+        Debug.DrawLine(bottomLeft, bottomRight, Color.red, 1f);
+        Debug.DrawLine(bottomRight, topRight, Color.red, 1f);
+        Debug.DrawLine(topRight, topLeft, Color.red, 1f);
+        Debug.DrawLine(topLeft, bottomLeft, Color.red, 1f);
     }
 }
