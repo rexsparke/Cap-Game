ca766fd40f91472c7d45dbe78d85724c02437e6e
rexsparke <kirito5115101@gmail.com>
2025-11-08 22:35:43 -0500

    did alot of stuff

--- Assets/scripts/pauseScreen.cs
+++ Assets/scripts/pauseScreen.cs
@@ -1,30 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class pauseScreen : MonoBehaviour
 {
     public GlobalManager globalManager;
 
+    bool inScreen = false;
     GameObject pauseGroupObjects;
 
     void Start()
     {
-        
+        pauseGroupObjects = this.gameObject;
     }
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
-            if(globalManager.isPaused == true)
+            if (!inScreen)
             {
-
+                OpenPauseScreen();
             }
-            pauseGroupObjects.transform.localPosition = new Vector3(pauseGroupObjects.transform.localPosition.x + 2000, pauseGroupObjects.transform.localPosition.y, 0f);
+            //Closes UI screen while in UI screen
+            else
+            {
+                ClosePauseScreen();
+            }
         }
     }
-    public void resumeButton_OnClick()
+    public void OpenPauseScreen()
+    {
+        globalManager.isPaused = true;
+        inScreen = true;
+        Time.timeScale = 0f;
+        pauseGroupObjects.transform.localPosition = new Vector3(pauseGroupObjects.transform.localPosition.x - 1950, pauseGroupObjects.transform.localPosition.y, 0f);
+    }
+    public void ClosePauseScreen()
+    {
+        globalManager.isPaused = false;
+        inScreen = false;
+        Time.timeScale = 1f;
+        pauseGroupObjects.transform.localPosition = new Vector3(pauseGroupObjects.transform.localPosition.x + 1950, pauseGroupObjects.transform.localPosition.y, 0f);
+
+    }
+    public void option_OnClick()
     {
 
+    }
+    public void returnToMenut_OnClick()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void exit_OnClick()
+    {
+        Application.Quit();
     }
 }
