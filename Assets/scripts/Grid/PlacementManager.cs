45b4639c9e7a87be1a2bf40bfc7fd897ea23791f
rexsparke <kirito5115101@gmail.com>
2025-12-07 19:25:12 -0500

    build fixs

--- Assets/scripts/Grid/PlacementManager.cs
+++ Assets/scripts/Grid/PlacementManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.U2D.Aseprite;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
